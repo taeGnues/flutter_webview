9063e63ccb134af7f9ee99a9a9ae4f677c8ed883
태승 <gnues@taeseung-ui-MacBookAir.local>
2023-02-09 01:02:57 +0900

    Webview 4.0 and Complete WebApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'screen/home_screen.dart';
 
 void main() {
+  // flutter 프레임워크가 앱 실행이 준비될때까지 기다린다.
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     MaterialApp(
       home: HomeScreen(),
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,19 +2,44 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://die-will-coding.tistory.com/'); // string 값을 Uri 클래스로 변경
+// Uri는 Url를 쉽게 쓸수 있게 만드는 클래스다.
+
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  WebViewController controller =
+      WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..loadRequest(homeUrl); // final로 변수를 선언하지 않았음. (null이 될 수있음)
+
+  HomeScreen({Key? key}) : super(key: key); // const를 지워줘야함.
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('hello'),
+        backgroundColor: Colors.black,
+        title: Text('곧죽어도 콛잉'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              controller.loadRequest(homeUrl);
+            },
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+        ],
       ),
-      body: WebView(
-        initialUrl: 'https://die-will-coding.tistory.com/',
-        javascriptMode: JavascriptMode.unrestricted,
+      body: WebViewWidget(
+        controller: controller,
       ),
+      // WebView(
+      //   onWebViewCreated: (WebViewController controller) {
+      //     this.controller = controller; // controller에 저장.
+      //   }, // 생성 시 웹뷰컨트롤 사용
+      //   initialUrl: homeUrl,
+      //   javascriptMode: JavascriptMode.unrestricted,
+      // ),
     );
   }
 }
